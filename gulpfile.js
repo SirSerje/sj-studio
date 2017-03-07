2d24dac9819a2c9ba47b454d056da066da9d35f6
SirSerje <sir.serje@gmail.com>
2017-03-08 00:54:27 +0200

    #7 #8 gulp-jade, gulp-ftp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
+//----------------requires----------------
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
+var gutil = require( 'gulp-util' );
+var ftp = require( 'vinyl-ftp' );
+//---------------end requires--------------
 
 var input = './sass/**/*.scss';
 var output = './test';
-
 var input1 = './css/main/style.scss';
 var output1 = './css/main/';
 
-var jadeinput = "*.jade";
-var jadeoutput = "./jadeTest/";
+var jadeInput = "*.jade";
+var jadeOutput = "./jadeTest/";
 
+var spaceportDestination = '/sj-photostudio.com/spaceport';
 var sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded' /* compressed expanded */
@@ -30,12 +34,38 @@ gulp.task('sass-main', function () {
         .pipe(gulp.dest(output1));
 });
 
-gulp.task('jadetest', function() {
+gulp.task('jade-test', function() {
     var YOUR_LOCALS = {};
 
-    gulp.src(jadeinput)
+    gulp.src(jadeInput)
         .pipe(jade({
             locals: YOUR_LOCALS
         }))
-        .pipe(gulp.dest(jadeoutput))
+        .pipe(gulp.dest(jadeOutput))
 });
+
+gulp.task('spaceport-deploy', function () {
+//TODO WARINNG!!! do not commit with real login / password
+    var conn = ftp.create( {
+        host:     'XXXX',              //insert host
+        user:     'XXXX',              //insert user
+        password: 'XXXX',             //insert pass
+        parallel: 10,
+        log:      gutil.log
+    } );
+
+    var globs = [
+        'index-main.html',
+        'js/**',
+        'include/**',
+        'images/**',
+        'css/**'
+    ];
+
+    // using base = '.' will transfer everything to /public_html correctly
+    // turn off buffering in gulp.src for best performance
+
+    return gulp.src( globs, { base: '.', buffer: false } )
+        //.pipe( conn.newer( '/public_html' ) ) // only upload newer files
+        .pipe(conn.dest(spaceportDestination));
+} );
