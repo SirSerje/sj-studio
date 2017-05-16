0ae128111f2e0a7bb7cf246e4dcab6f9321f718f
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2017-05-16 21:19:30 +0300

    #58_edit_email_form

--- include/sendemail.php
+++ include/sendemail.php
@@ -5,7 +5,7 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'username@website.com', // Your Email Address
+				'email' => 'roman.alexeychenko@gmail.com', // Your Email Address
 				'name' => 'Your Name' // Your Name
 			);
 
